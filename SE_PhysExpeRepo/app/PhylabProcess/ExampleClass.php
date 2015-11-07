7b2f29f47c40c629e108381906c2e6486d6e426a
项塔兰 <qianlxc@126.com>
2015-11-07 22:22:18 +0800

    delete SE_Phy_lab

--- SE_PhysExpeRepo/app/PhylabProcess/ExampleClass.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\PhylabProcess;
-class ExampleClass{
-    public function info(){
-        echo "its a ExampleClass";
-    }
-}
-?>
--- SE_PhysExpeRepo/resources/views/errors/phylab.blade.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-</head>
-<body>
-    <h1>error: {{$message}}, code: {{$code}}, httpCode: {{$httpCode}}</h1>
-</body>
-</html>
