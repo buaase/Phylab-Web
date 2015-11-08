5503fb741207b7a891a9b1582a4f94304fef7336
HoerWing <root@xelnaga.org>
2015-11-08 20:04:08 +0800

    我快饿死了。。。。

--- SE_PhysExpeRepo/public/js/regist.js
+++ SE_PhysExpeRepo/public/js/regist.js
@@ -1,42 +1,32 @@
 ﻿	
-		function check(){
-			SetDisable('btn-Signup',true);
-		}
-		$(function () {
-			$('[data-toggle="popover"]').popover({
-				html : true
-			});
-		})
-		
 		$('.user-input').bind("change",function(){
-			browseApp = browser();
-			if(browseApp!="FF"||browseApp!="Safari"||browseApp!="Chrome")
-				patterns = this.title;
-			else
-				patterns = this.pattern;
-			if(this.id=="CheckPwd"){
-				if(this.value!=$('#InputPwd')[0].value){
-					$(this).addClass("wrong-input");
-					_setShowHide(this.id+'Alert',this.id+'Success')();
-				}
-				else{
-					$(this).removeClass("wrong-input");
-					_setShowHide(this.id+'Success',this.id+'Alert')();
-				}
-			}
-			else if(!(new RegExp(patterns)).test(this.value)){
-				$(this).addClass("wrong-input");
-				_setShowHide(this.id+'Alert',this.id+'Success')();
-			}
-			else{
-				$(this).removeClass("wrong-input");
-				_setShowHide(this.id+'Success',this.id+'Alert')();
-			}
-		});	
-		function setSignUpStatus(){
-			if(document.getElementById('CheckLicense').checked) SetDisable('btn-Signup',false);
-			else SetDisable('btn-Signup',true);
-		}	
+            if(this.id=="CheckPwd"){
+                if(this.value!=$('#InputPwd')[0].value){
+                    $(this).addClass("wrong-input");
+                    _setShowHide(this.id+'Alert',this.id+'Success')();
+                }
+                else{
+                    $(this).removeClass("wrong-input");
+                    _setShowHide(this.id+'Success',this.id+'Alert')();
+                }
+            }
+            else if(!(new RegExp(this.pattern)).test(this.value)){
+                $(this).addClass("wrong-input");
+                _setShowHide(this.id+'Alert',this.id+'Success')();
+            }
+            else{
+                $(this).removeClass("wrong-input");
+                _setShowHide(this.id+'Success',this.id+'Alert')();
+            }
+        });
+        
+        function check(){
+            SetDisable('btn-Signup',true);
+        }
+        function setSignUpStatus(){
+            if(document.getElementById('CheckLicense').checked) SetDisable('btn-Signup',false);
+            else SetDisable('btn-Signup',true);
+        }
 		function signUp(){
 			var check = true;
 			$('.user-input').each(function(){
@@ -46,8 +36,10 @@
 					_setShowHide(this.id+'Alert',this.id+'Success')();
 				}
 			});
-			return check;
+			$('#register-post').click();
 		}
-	}
+		$(function () {
+            $('[data-toggle="popover"]').popover();
+        })
 		
 	
