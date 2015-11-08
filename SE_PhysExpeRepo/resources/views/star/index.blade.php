b00f19ec1051893d829807447288dce099c410a0
HoerWing <root@xelnaga.org>
2015-11-08 21:00:09 +0800

    GG 我傻逼

--- SE_PhysExpeRepo/resources/views/star/index.blade.php
+++ SE_PhysExpeRepo/resources/views/star/index.blade.php
@@ -22,7 +22,7 @@
                 <tr id="star_{{$stars[$i]['id']}}">
                     <th scope="row">{{$i}}</th>
                     <td>{{$stars[$i]["name"]}}</td>
-                    <td><button type="button" class="btn btn-default" onclick="window.open('{{URL::route('star').'/'.$stars[$i]['id']}}')"><a href="{{URL::route('star').'/'.$stars[$i]['id']}}">查看</a></button></td>
+                    <td><button type="button" class="btn btn-default" onclick="window.open('{{URL::route('star').'/'.$stars[$i]['id']}}')"><a href="#">查看</a></button></td>
                     <td>{{$stars[$i]["time"]}}</td>
                     <td><button type="button" class="btn btn-primary" onclick="deleteStar('{{$stars[$i]['id']}}')">删除</button>
                 </tr>
