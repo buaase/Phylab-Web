51f654d529a6aa3ffe1b0b0e699efca57e19ac04
kibbon <504037668@qq.com>
2015-12-17 23:19:32 +0800

    delete

--- SE_PhysExpeRepo/resources/views/user/edit.blade.php
+++
@@ -1,7 +0,0 @@
- <form method="POST" action="/user/avatar" enctype="multipart/form-data">
-
-    <input type="file" name="avatar" />
-
-    <input type="submit" name="submit" value="Submit" />
-
-</form> 
--- SE_PhysExpeRepo/storage/app/script/create.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/lib;cd $1;python $2 $3 $4;exit;
